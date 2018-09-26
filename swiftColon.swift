73680356bb78659eeb31f9dc8c2eda5a24ad075d
tokorom <tokorom@gmail.com>
2018-09-26 19:03:38 +0900

    Add samples

--- swiftColon.swift
+++ swiftColon.swift
@@ -5,13 +5,29 @@ enum Parameter {
     case int(int: Int)
 }
 
+protocol Container {
+    associatedtype Item
+}
+
 class Foo: NSObject {
     let params: [Parameter]
-    let tuple: (Int, String)
+    let tuple: (lhs: Int, rhs: String)
 
-    init(paramas: [Parameter], tuple: (Int, String)) {
+    init(params: [Parameter], tuple: (lhs: Int, rhs: String)) {
         self.params = params
         self.tuple = tuple
+    }
+
+    func say(message: String) {
+        print(message)
+    }
+
+    func say(messages: [String]) {
+        print(messages)
+    }
+
+    func gen<T: Container>(param: T) -> T {
+        return param
     }
 }
 
@@ -20,8 +36,26 @@ let foo = Foo(
         .string(string: "hello"),
         .int(int: 100),
     ],
-    tuple: (100, "hello")
+    tuple: (lhs: 100, rhs: "hello")
 )
+
+let bar = "bar"
+let baz = "baz"
+
+foo.say(message: bar + baz)
 
 print(foo.params)
 print(foo.tuple)
+
+let param = Parameter.int(int: 5)
+switch param {
+case .string(let string):
+    print(string)
+case .int(let int):
+    print(int)
+}
+
+func allItemsMatch<C1: Container, C2: Container> (_ someContainer: C1, _ anotherContainer: C2) -> Bool
+    where C1.Item == C2.Item, C1.Item: Equatable {
+        return true
+}
